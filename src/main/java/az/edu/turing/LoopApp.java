c29d941ffeeb4a41bc0951e1b1b66a49839923ea
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-11-01 17:53:18 +0400

    make conditon

--- src/main/java/az/edu/turing/LoopApp.java
+++ src/main/java/az/edu/turing/LoopApp.java
@@ -3,24 +3,26 @@ package az.edu.turing;
 import java.util.Scanner;
 
 public class LoopApp {
-    public static void main(String[]args){
+
+    public static void main(String[]args) {
         //input
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter number:");
-        int number=scanner.nextInt();
+        int number = scanner.nextInt();
         //prosess
-        if (number%2==1){
-            System.out.println("Number is odd");
-        }else if(number%2==0 && number!=0){
-            System.out.println("Number is even");
-        }else if(number<0){
+        if (number < 0)
             System.out.println("Number is negative");
-        }else if(number>0){
+            else if (number > 0)
             System.out.println("Number is pozitiv");
-        }else if(number==0){
-            System.out.println("Number equal to zero");
-        }
-        //output
+            if (number % 2 == 1)
+                System.out.println("Number is odd");
+            if (number % 2 == 0 && number != 0)
+                System.out.println("Number is even");
+            if (number == 0)
+                System.out.println("Number equal to zero");
 
+            //output
+
+        }
     }
-}
+
--- src/main/java/az/edu/turing/LoopApp2.java
+++ src/main/java/az/edu/turing/LoopApp2.java
@@ -1,4 +1,14 @@
 package az.edu.turing;
 
+import java.util.Scanner;
+
 public class LoopApp2 {
+
+    public static void main(String[]args){
+        //input
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        //prosess
+
+    }
 }
